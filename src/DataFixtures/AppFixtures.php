01d564868fb9370edda6196b9b5a5a82911600a6
Fabien V <fabien.vermeiren@gmail.com>
2022-06-16 09:56:03 +0200

    fix password hash in fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,20 +2,32 @@
 
 namespace App\DataFixtures;
 
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
 
 
+
 class AppFixtures extends Fixture
 {
+    private $passwordHasher;
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         $user = new User();
         $user->setUsername('user1');
         $user->setRoles(['ROLE_USER']);
-        $user->setPassword('1111');
+        $plaintextPassword = '1111';
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
         $manager->persist($user);
         $manager->flush();
     }
